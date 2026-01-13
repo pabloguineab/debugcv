1aafa5f582b7694a3e123abc1d366c6f3918962d
Pablo Guinea <pabloguineab@gmail.com>
2026-01-13 13:48:01 +0100

    Fix LordIcon to fetch JSON from URL before rendering

--- src/components/icons/lord-icon.tsx
+++ src/components/icons/lord-icon.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Player } from '@lordicon/react';
-import { useRef } from 'react';
+import { useRef, useEffect, useState } from 'react';
 
 interface LordIconProps {
     src: string;
@@ -17,28 +17,41 @@ export function LordIcon({
     colors
 }: LordIconProps) {
     const playerRef = useRef<Player>(null);
+    const [iconData, setIconData] = useState<any>(null);
+
+    // Load icon JSON from URL
+    useEffect(() => {
+        fetch(src)
+            .then(res => res.json())
+            .then(data => setIconData(data))
+            .catch(err => console.error('Failed to load icon:', err));
+    }, [src]);
 
     const handleMouseEnter = () => {
-        if (trigger === 'hover') {
-            playerRef.current?.playFromBeginning();
+        if (trigger === 'hover' && playerRef.current) {
+            playerRef.current.playFromBeginning();
         }
     };
 
     const handleClick = () => {
-        if (trigger === 'click') {
-            playerRef.current?.playFromBeginning();
+        if (trigger === 'click' && playerRef.current) {
+            playerRef.current.playFromBeginning();
         }
     };
+
+    if (!iconData) {
+        return <div style={{ width: size, height: size }} />;
+    }
 
     return (
         <div
             onMouseEnter={handleMouseEnter}
             onClick={handleClick}
-            style={{ width: size, height: size, display: 'inline-block' }}
+            style={{ width: size, height: size, display: 'inline-flex', alignItems: 'center', justifyContent: 'center' }}
         >
             <Player
                 ref={playerRef}
-                icon={src}
+                icon={iconData}
                 size={size}
                 colors={colors}
             />
